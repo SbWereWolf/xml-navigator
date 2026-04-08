846a9f73b45c8c942773c7cce6ba33d3220d92cc
SbWereWolf <ulfnew@gmail.com>
2026-04-08 05:09:07 +0500

    Fix PHP 7.0 CI checks

--- src/SbWereWolf/XmlNavigator/Extraction/PrettyPrintComposer.php
+++ src/SbWereWolf/XmlNavigator/Extraction/PrettyPrintComposer.php
@@ -271,6 +271,9 @@ class PrettyPrintComposer implements Notation
         return null;
     }
 
+    /**
+     * @param array<mixed, mixed> $value
+     */
     private static function isList(array $value): bool
     {
         return $value === array_values($value);
